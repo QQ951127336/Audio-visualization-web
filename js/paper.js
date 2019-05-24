4f7886142b51c8583fbde3ef3fad4c55802e274e
max <951127336@qq.com>
2019-05-24 09:22:46 +0800

    纸张变化效果

---
+++ js/paper.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    // $("#paperBox1").hide();
+    $("#paperBox2").hide();
+    $("#paperBox3").hide();
+    $("#paperBox4").hide();
+
+    //test
+    $("#paper1").click(function(){
+        paperBegin();
+    });
+
+    function paperBegin() {
+        paperHalf();
+    }
+
+    function paperHalf() {
+        $("#paperBox1").hide();
+        $("#paperBox2").fadeIn("slow",function() {
+            paperFinished();
+        });
+    }
+
+    function paperFinished() {
+        $("#paperBox2").hide();
+        $("#paperBox3").fadeIn("slow",function () {
+            buttonShow();
+        });
+    }
+
+    function buttonShow() {
+        $("#paperBox4").fadeIn();
+    }
+});
