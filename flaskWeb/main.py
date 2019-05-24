23723b9c8d0dea27491a5b08cac1771c43426afa
max <951127336@qq.com>
2019-05-24 10:41:24 +0800

    add sqlite db

--- flaskWeb/main.py
+++ flaskWeb/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
@@ -10,3 +12,16 @@ def home():
 
 if __name__ == '__main__':
     app.run()
+
+class FeelData(db.Model):
+    __tablename__ = 'feel_data'
+    id = db.Column(db.Integer(), primary_key=True)
+    dislike = db.Column('dislike', db.Integer())
+    like = db.Column('like', db.Integer())
+
+    def __init__(self):
+        self.dislike = 0
+        self.like = 0
+
+    def __repr__(self):
+        return "<Data '{} {}'>".format(self.like, self.dislike)
